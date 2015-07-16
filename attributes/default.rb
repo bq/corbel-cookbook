01ef61dcaa2a3c0b4975e78a3eba519e5d2f7257
Ismael Madirolas <ismael.madirolas@bq.com>
2015-07-16 17:54:45 +0200

    delete default_level variable

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,4 @@
 #Module defaults
-default_level = "INFO"
-
 default[:corbel][:iam][:type] = "tarball"
 default[:corbel][:iam][:artifact_id] = "iam"
 default[:corbel][:iam][:group_id] = "com.bq.oss.corbel"
@@ -12,11 +10,11 @@ default[:corbel][:iam][:docker_link] = "mongo:mongo, redis:redis, rabbitmq:rabbi
 default[:corbel][:iam][:docker_ports] = "8082:8082, 8083:8083"
 default[:corbel][:iam][:scripts] = ['scripts/iam/iam-scopes.groovy', 'scripts/iam/evci-scopes.groovy', 'scripts/iam/webfs-scopes.groovy', 'scripts/iam/composite-scopes.groovy']
 default[:corbel][:iam][:log][:file][:enabled] = true
-default[:corbel][:iam][:log][:file][:level] = default_level
+default[:corbel][:iam][:log][:file][:level] = "INFO"
 default[:corbel][:iam][:log][:file][:currentLogFilename] = "/var/log/silkroad/iam.log"
 default[:corbel][:iam][:log][:file][:archivedLogFilenamePattern] = "/var/log/silkroad/iam-%d.log.gz"
 default[:corbel][:iam][:log][:syslog][:enabled] = true
-default[:corbel][:iam][:log][:syslog][:level] = default_level
+default[:corbel][:iam][:log][:syslog][:level] = "INFO"
 
 
 default[:corbel][:resources][:type] = "tarball"
@@ -29,11 +27,11 @@ default[:corbel][:resources][:docker_image] = "corbel/resources"
 default[:corbel][:resources][:docker_link] = "mongo:mongo, redis:redis, rabbitmq:rabbitmq, elasticsearch:elasticsearch"
 default[:corbel][:resources][:docker_ports] = "8080:8080, 8081:8081"
 default[:corbel][:resources][:log][:file][:enabled] = true
-default[:corbel][:resources][:log][:file][:level] = default_level
+default[:corbel][:resources][:log][:file][:level] = "INFO"
 default[:corbel][:resources][:log][:file][:currentLogFilename] = "/var/log/silkroad/resources.log"
 default[:corbel][:resources][:log][:file][:archivedLogFilenamePattern] = "/var/log/silkroad/resources-%d.log.gz"
 default[:corbel][:resources][:log][:syslog][:enabled] = true
-default[:corbel][:resources][:log][:syslog][:level] = default_level
+default[:corbel][:resources][:log][:syslog][:level] = "INFO"
 
 
 default[:corbel][:webfs][:type] = "tarball"
@@ -46,11 +44,11 @@ default[:corbel][:webfs][:docker_image] = "corbel/webfs"
 default[:corbel][:webfs][:docker_link] = "mongo:mongo, redis:redis, rabbitmq:rabbitmq"
 default[:corbel][:webfs][:docker_ports] = "8096:8096, 8097:8097"
 default[:corbel][:webfs][:log][:file][:enabled] = true
-default[:corbel][:webfs][:log][:file][:level] = default_level
+default[:corbel][:webfs][:log][:file][:level] = "INFO"
 default[:corbel][:webfs][:log][:file][:currentLogFilename] = "/var/log/silkroad/webfs.log"
 default[:corbel][:webfs][:log][:file][:archivedLogFilenamePattern] = "/var/log/silkroad/webfs-%d.log.gz"
 default[:corbel][:webfs][:log][:syslog][:enabled] = true
-default[:corbel][:webfs][:log][:syslog][:level] = default_level
+default[:corbel][:webfs][:log][:syslog][:level] = "INFO"
 
 default[:corbel][:evci][:type] = "tarball"
 default[:corbel][:evci][:artifact_id] = "evci"
@@ -62,11 +60,11 @@ default[:corbel][:evci][:docker_image] = "corbel/evci"
 default[:corbel][:evci][:docker_link] = "mongo:mongo, redis:redis, rabbitmq:rabbitmq"
 default[:corbel][:evci][:docker_ports] = "8086:8086, 8087:8087"
 default[:corbel][:evci][:log][:file][:enabled] = true
-default[:corbel][:evci][:log][:file][:level] = default_level
+default[:corbel][:evci][:log][:file][:level] = "INFO"
 default[:corbel][:evci][:log][:file][:currentLogFilename] = "/var/log/silkroad/evci.log"
 default[:corbel][:evci][:log][:file][:archivedLogFilenamePattern] = "/var/log/silkroad/evci-%d.log.gz"
 default[:corbel][:evci][:log][:syslog][:enabled] = true
-default[:corbel][:evci][:log][:syslog][:level] = default_level
+default[:corbel][:evci][:log][:syslog][:level] = "INFO"
 
 #JAVA defaults
 default[:java][:install_flavor] = "oracle"
