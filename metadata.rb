4190668352cd5f7bee63fd4f4aa71508bacf67ce
Alexander De Leon <me@alexdeleon.name>
2015-07-28 10:22:40 +0200

    Bumped version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Alexander De Leon'
 maintainer_email 'alex.deleon@devialab.com'
 license          'MIT'
 description      'Installs/Configures Corbel platform'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'tar'
 depends 'java'
