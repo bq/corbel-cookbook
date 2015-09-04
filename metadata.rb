9b99ae619a271e7aa4a70ac1ec9c83f6d89bdd3d
Alberto J. Rubio <yebenes9@gmail.com>
2015-09-04 16:31:34 +0200

    Bumped version to 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Alexander De Leon'
 maintainer_email 'alex.deleon@devialab.com'
 license          'MIT'
 description      'Installs/Configures Corbel platform'
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'tar'
 depends 'java'
