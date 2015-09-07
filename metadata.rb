98076f8a330f3b8d504de2e5cbea3bc5582d2db5
Alexander De Leon <me@alexdeleon.name>
2015-09-07 07:36:13 -0430

    Added support for controlling JVM parameters.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Alexander De Leon'
 maintainer_email 'alex.deleon@devialab.com'
 license          'MIT'
 description      'Installs/Configures Corbel platform'
-version          '0.2.1'
+version          '0.2.2'
 
 depends 'tar'
 depends 'java'
